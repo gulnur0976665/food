598f637e430dd3455b912f72d04a93ae89af7cfa
Gulnur Turdumamatova <gulnurturdumamatova84@gmail.com>
2025-09-01 10:46:42 +0600

    buttu

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -4,7 +4,7 @@ import type { IProduct } from "../../types/Types";
 import scss from "./Category.module.scss";
 import { setActiveButton } from "../../redux/productSlice";
 import Footer from "../footer/Footer";
-
+import img from "../../assets/empty-cart.png";
 const Category = () => {
   const nav = useNavigate();
   const dispatch = useAppDispatch();
@@ -22,91 +22,97 @@ const Category = () => {
       {" "}
       <div className={scss.Category}>
         {filterCategory.length === 0 ? (
-          <p>Продукттар жок</p>
-        ) : (
-          <div className={scss.categoryBlock}>
-            <button
-              className={scss.backBtn}
-              onClick={() => {
-                nav("/");
-                dispatch(setActiveButton(""));
-              }}
-            >
-              Главная
-            </button>
-
-            <div className={scss.content}>
-              {title === "Блюда" ? (
-                dishes.map((dish) => (
-                  <div key={dish} className={scss.dishBlock}>
-                    {filterCategory.some(
-                      (el: IProduct) => el.dishes === dish
-                    ) && (
-                      <div className={scss.hrText}>
-                        <hr />
-                        <h6 className={scss.categoryTitle}>Блюда с {dish}</h6>
-                        <hr />
-                      </div>
-                    )}
-
-                    <div className={scss.productList}>
-                      {filterCategory
-                        .filter((el: IProduct) => el.dishes === dish)
-                        .map((el: IProduct) => (
-                          <div key={el.id} className={scss.productCard}>
-                            <img
-                              src={el.url}
-                              alt={el.name}
-                              className={scss.productImage}
-                            />
-                            <div className={scss.texts}>
-                              <p className={scss.productPrice}>
-                                {el.price} сом
-                              </p>
-                              <p className={scss.productName}>{el.name}</p>
-                              {el.portions && (
-                                <p className={scss.productPortions}>
-                                  от {el.portions} порции
-                                </p>
-                              )}
-                              <p className={scss.productDes}>{el.descripton}</p>
-                            </div>
-                          </div>
-                        ))}
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <>
-                  <h2 className={scss.categoryTitle2}>{title}</h2>
-                  <div className={scss.productList}>
-                    {filterCategory.map((el: IProduct) => (
-                      <div key={el.id} className={scss.productCard}>
-                        <img
-                          src={el.url}
-                          alt={el.name}
-                          className={scss.productImage}
-                        />
-                        <div className={scss.texts}>
-                          <p className={scss.productPrice}>{el.price} сом</p>
-                          <p className={scss.productName}>{el.name}</p>
-                          {el.portions && (
-                            <p className={scss.productPortions}>
-                              от {el.portions} порции
-                            </p>
-                          )}
-                          <p className={scss.productDes}>{el.descripton}</p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </>
-              )}
-            </div>
+          <div className={scss.IMG}>
+            <img src={img} alt="" className={scss.emptyImg} />
           </div>
+        ) : (
+          <>
+            <div className={scss.categoryBlock}>
+              <button
+                className={scss.backBtn}
+                onClick={() => {
+                  nav("/");
+                  dispatch(setActiveButton(""));
+                }}
+              >
+                Главная
+              </button>
+
+              <div className={scss.content}>
+                {title === "Блюда" ? (
+                  dishes.map((dish) => (
+                    <div key={dish} className={scss.dishBlock}>
+                      {filterCategory.some(
+                        (el: IProduct) => el.dishes === dish
+                      ) && (
+                        <div className={scss.hrText}>
+                          <hr />
+                          <h6 className={scss.categoryTitle}>Блюда с {dish}</h6>
+                          <hr />
+                        </div>
+                      )}
+
+                      <div className={scss.productList}>
+                        {filterCategory
+                          .filter((el: IProduct) => el.dishes === dish)
+                          .map((el: IProduct) => (
+                            <div key={el.id} className={scss.productCard}>
+                              <img
+                                src={el.url}
+                                alt={el.name}
+                                className={scss.productImage}
+                              />
+                              <div className={scss.texts}>
+                                <p className={scss.productPrice}>
+                                  {el.price} сом
+                                </p>
+                                <p className={scss.productName}>{el.name}</p>
+                                {el.portions && (
+                                  <p className={scss.productPortions}>
+                                    от {el.portions} порции
+                                  </p>
+                                )}
+                                <p className={scss.productDes}>
+                                  {el.descripton}
+                                </p>
+                              </div>
+                            </div>
+                          ))}
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <>
+                    <h2 className={scss.categoryTitle2}>{title}</h2>
+                    <div className={scss.productList}>
+                      {filterCategory.map((el: IProduct) => (
+                        <div key={el.id} className={scss.productCard}>
+                          <img
+                            src={el.url}
+                            alt={el.name}
+                            className={scss.productImage}
+                          />
+                          <div className={scss.texts}>
+                            <p className={scss.productPrice}>{el.price} сом</p>
+                            <p className={scss.productName}>{el.name}</p>
+                            {el.portions && (
+                              <p className={scss.productPortions}>
+                                от {el.portions} порции
+                              </p>
+                            )}
+                            <p className={scss.productDes}>{el.descripton}</p>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+            <Footer />
+          </>
         )}
       </div>
-      <Footer />
     </>
   );
 };
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -4,7 +4,7 @@ import type { IProduct } from "../../types/Types";
 import Footer from "../footer/Footer";
 import { setActiveButton } from "../../redux/productSlice";
 import { useNavigate } from "react-router-dom";
-
+import img from "../../assets/empty-cart.png";
 const Home = () => {
   const dispatch = useAppDispatch();
   const { product } = useAppSelector((s) => s.product);
@@ -14,124 +14,128 @@ const Home = () => {
     <>
       <div className={scss.homeContainer}>
         {product.length === 0 ? (
-          <div className={scss.loading}>Loading...</div>
-        ) : (
-          <div className={scss.content}>
-            <div className={scss.categoryBlock}>
-              <h2 className={scss.categoryTitle}>Салаты</h2>
-              <div className={scss.productList}>
-                {product
-                  .filter((el: IProduct) => el.category === "Салаты")
-                  .slice(0, 4)
-                  .map((el: IProduct) => (
-                    <div key={el.id} className={scss.productCard}>
-                      <img
-                        src={el.url}
-                        alt={el.name}
-                        className={scss.productImage}
-                      />
-                      <div className={scss.texts}>
-                        <p className={scss.productPrice}>{el.price} сом</p>
-                        <p className={scss.productName}>{el.name}</p>
-                        {el.portions && (
-                          <p className={scss.productPortions}>
-                            от {el.portions} порции
-                          </p>
-                        )}
-                      </div>
-                    </div>
-                  ))}
-              </div>
-              <div className={scss.btnHr}>
-                <button
-                  onClick={() => {
-                    dispatch(setActiveButton("Салаты"));
-                    nav(`/category/${"Салаты"}`);
-                  }}
-                >
-                  Подробнее
-                </button>
-                <hr />
-              </div>
-            </div>
-
-            <div className={scss.categoryBlock}>
-              <h2 className={scss.categoryTitle}>Супы</h2>
-              <div className={scss.productList}>
-                {product
-                  .filter((el: IProduct) => el.category === "Супы")
-                  .map((el: IProduct) => (
-                    <div key={el.id} className={scss.productCard}>
-                      <img
-                        src={el.url}
-                        alt={el.name}
-                        className={scss.productImage}
-                      />
-                      <div className={scss.texts}>
-                        <p className={scss.productPrice}>{el.price} сом</p>
-                        <p className={scss.productName}>{el.name}</p>
-                        {el.portions && (
-                          <p className={scss.productPortions}>
-                            от {el.portions} порции
-                          </p>
-                        )}
-                      </div>
-                    </div>
-                  ))}
-              </div>
-              <div className={scss.btnHr}>
-                <button
-                  onClick={() => {
-                    dispatch(setActiveButton("Супы"));
-                    nav(`/category/${"Супы"}`);
-                  }}
-                >
-                  Подробнее
-                </button>
-                <hr />
-              </div>
-            </div>
-
-            <div className={scss.categoryBlock}>
-              <h2 className={scss.categoryTitle}>Горячие Блюда</h2>
-              <div className={scss.productList}>
-                {product
-                  .filter((el: IProduct) => el.category === "Блюда")
-                  .map((el: IProduct) => (
-                    <div key={el.id} className={scss.productCard}>
-                      <img
-                        src={el.url}
-                        alt={el.name}
-                        className={scss.productImage}
-                      />
-                      <div className={scss.texts}>
-                        <p className={scss.productPrice}>{el.price} сом</p>
-                        <p className={scss.productName}>{el.name}</p>
-                        {el.portions && (
-                          <p className={scss.productPortions}>
-                            от {el.portions} порции
-                          </p>
-                        )}
-                      </div>
-                    </div>
-                  ))}
-              </div>
-              <div className={scss.btnHr}>
-                <button
-                  onClick={() => {
-                    dispatch(setActiveButton("Блюда"));
-                    nav(`/category/${"Блюда"}`);
-                  }}
-                >
-                  Подробнее
-                </button>
-                <hr />
-              </div>
-            </div>
+          <div className={scss.IMG}>
+            <img src={img} alt="" className={scss.emptyImg} />
           </div>
+        ) : (
+          <>
+            <div className={scss.content}>
+              <div className={scss.categoryBlock}>
+                <h2 className={scss.categoryTitle}>Салаты</h2>
+                <div className={scss.productList}>
+                  {product
+                    .filter((el: IProduct) => el.category === "Салаты")
+                    .slice(0, 4)
+                    .map((el: IProduct) => (
+                      <div key={el.id} className={scss.productCard}>
+                        <img
+                          src={el.url}
+                          alt={el.name}
+                          className={scss.productImage}
+                        />
+                        <div className={scss.texts}>
+                          <p className={scss.productPrice}>{el.price} сом</p>
+                          <p className={scss.productName}>{el.name}</p>
+                          {el.portions && (
+                            <p className={scss.productPortions}>
+                              от {el.portions} порции
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                </div>
+                <div className={scss.btnHr}>
+                  <button
+                    onClick={() => {
+                      dispatch(setActiveButton("Салаты"));
+                      nav(`/category/${"Салаты"}`);
+                    }}
+                  >
+                    Подробнее
+                  </button>
+                  <hr />
+                </div>
+              </div>
+
+              <div className={scss.categoryBlock}>
+                <h2 className={scss.categoryTitle}>Супы</h2>
+                <div className={scss.productList}>
+                  {product
+                    .filter((el: IProduct) => el.category === "Супы")
+                    .map((el: IProduct) => (
+                      <div key={el.id} className={scss.productCard}>
+                        <img
+                          src={el.url}
+                          alt={el.name}
+                          className={scss.productImage}
+                        />
+                        <div className={scss.texts}>
+                          <p className={scss.productPrice}>{el.price} сом</p>
+                          <p className={scss.productName}>{el.name}</p>
+                          {el.portions && (
+                            <p className={scss.productPortions}>
+                              от {el.portions} порции
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                </div>
+                <div className={scss.btnHr}>
+                  <button
+                    onClick={() => {
+                      dispatch(setActiveButton("Супы"));
+                      nav(`/category/${"Супы"}`);
+                    }}
+                  >
+                    Подробнее
+                  </button>
+                  <hr />
+                </div>
+              </div>
+
+              <div className={scss.categoryBlock}>
+                <h2 className={scss.categoryTitle}>Горячие Блюда</h2>
+                <div className={scss.productList}>
+                  {product
+                    .filter((el: IProduct) => el.category === "Блюда")
+                    .map((el: IProduct) => (
+                      <div key={el.id} className={scss.productCard}>
+                        <img
+                          src={el.url}
+                          alt={el.name}
+                          className={scss.productImage}
+                        />
+                        <div className={scss.texts}>
+                          <p className={scss.productPrice}>{el.price} сом</p>
+                          <p className={scss.productName}>{el.name}</p>
+                          {el.portions && (
+                            <p className={scss.productPortions}>
+                              от {el.portions} порции
+                            </p>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                </div>
+                <div className={scss.btnHr}>
+                  <button
+                    onClick={() => {
+                      dispatch(setActiveButton("Блюда"));
+                      nav(`/category/${"Блюда"}`);
+                    }}
+                  >
+                    Подробнее
+                  </button>
+                  <hr />
+                </div>
+              </div>
+            </div>
+            <Footer />
+          </>
         )}
       </div>
-      <Footer />
     </>
   );
 };
